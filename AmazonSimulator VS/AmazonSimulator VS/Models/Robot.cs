d1e59c2f9b82ffe7db919cc6bfd51409c1c1ed16
smit1712 <32259732+smit1712@users.noreply.github.com>
2018-09-12 13:00:47 +0200

    type=Robot

--- AmazonSimulator VS/AmazonSimulator VS/Models/Robot.cs
+++ AmazonSimulator VS/AmazonSimulator VS/Models/Robot.cs
@@ -10,7 +10,7 @@ namespace Models {
         bool HasRack = false;
         public Robot(double x, double y, double z, double rotationX, double rotationY, double rotationZ) : base(x,y,z,rotationX,rotationY,rotationZ)
         {
-            this.type = "robot";
+            this.type = "Robot";
             this.guid = Guid.NewGuid();
 
         }
